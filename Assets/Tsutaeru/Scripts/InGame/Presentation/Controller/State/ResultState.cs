02c0697054d869357bfedc1ace0357dfd35b145d
kitatas <315kitatas10703@gmail.com>
2023-05-24 20:33:16 +0900

    fix: 記録更新時のLoading表示

--- Assets/Tsutaeru/Scripts/InGame/Presentation/Controller/State/ResultState.cs
+++ Assets/Tsutaeru/Scripts/InGame/Presentation/Controller/State/ResultState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using Tsutaeru.Common;
@@ -9,6 +10,7 @@ namespace Tsutaeru.InGame.Presentation.Controller
 {
     public sealed class ResultState : BaseState
     {
+        private readonly LoadingUseCase _loadingUseCase;
         private readonly RankingUseCase _rankingUseCase;
         private readonly SceneUseCase _sceneUseCase;
         private readonly SoundUseCase _soundUseCase;
@@ -18,10 +20,11 @@ namespace Tsutaeru.InGame.Presentation.Controller
         private readonly RankingView _rankingView;
         private readonly UserRecordView _userRecordView;
 
-        public ResultState(RankingUseCase rankingUseCase, SceneUseCase sceneUseCase, SoundUseCase soundUseCase,
-            UserRecordUseCase userRecordUseCase, HintView hintView, ReloadButtonView reloadButtonView,
-            RankingView rankingView, UserRecordView userRecordView)
+        public ResultState(LoadingUseCase loadingUseCase, RankingUseCase rankingUseCase, SceneUseCase sceneUseCase,
+            SoundUseCase soundUseCase, UserRecordUseCase userRecordUseCase, HintView hintView,
+            ReloadButtonView reloadButtonView, RankingView rankingView, UserRecordView userRecordView)
         {
+            _loadingUseCase = loadingUseCase;
             _rankingUseCase = rankingUseCase;
             _sceneUseCase = sceneUseCase;
             _soundUseCase = soundUseCase;
@@ -47,17 +50,27 @@ namespace Tsutaeru.InGame.Presentation.Controller
             _soundUseCase.PlaySe(SeType.Hint);
             await _hintView.ResetAsync(UiConfig.ANIMATION_TIME, token);
 
+            // ロード表示
+            _loadingUseCase.Set(true);
+
             // ユーザーの記録更新 + ランキング送信
             await _userRecordUseCase.SendTimeAttackScoreAsync(token);
             var score = _userRecordUseCase.GetUserScore();
             _userRecordView.SetScore(score.current, score.high);
 
+            // ランキング反映待ち
+            await UniTask.Delay(TimeSpan.FromSeconds(0.5f), cancellationToken: token);
+
+            var records = await _rankingUseCase.GetTimeAttackRankingAsync(token);
+            _rankingView.SetUp(records);
+
+            // ロード非表示
+            _loadingUseCase.Set(false);
+
             _soundUseCase.PlaySe(SeType.Hint);
             await _hintView.RenderAsync("タイムアタック ランキング", UiConfig.ANIMATION_TIME, token);
             await _hintView.TweenHeightAsync(-50.0f, UiConfig.ANIMATION_TIME, token);
 
-            var records = await _rankingUseCase.GetTimeAttackRankingAsync(token);
-            _rankingView.SetUp(records);
             await _rankingView.ShowAsync(UiConfig.POPUP_TIME, token);
 
             await _reloadButtonView.ShowAsync(UiConfig.ANIMATION_TIME, token);
